4517fb6c24580a79902eb54bd64e02dd05a847ae
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-08-19 18:16:53 +0200

    fiix: fixed the env issues

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+const dbName = process.env.DATABASE_NAME || "ADMIN";
+const dbPassword = process.env.DATABASE_PASSWORD || "*****";
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
@@ -15,7 +17,7 @@ app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
 
-mongoose.connect("mongodb+srv://adk:Amdy300@cluster0.sazjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+mongoose.connect(`mongodb+srv://${dbName}:${dbPassword}@cluster0.sazjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(
     () => {
         console.log("connected to database");
     }
