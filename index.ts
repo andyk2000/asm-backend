a02198fbe083f551bac209db6fc6045746f1c45d
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-08-19 18:58:30 +0200

    feat: setup prettier eslint and husky

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import express, { Express, Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import express, { Express, Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -9,18 +9,21 @@ const port = process.env.PORT || 8000;
 const dbName = process.env.DATABASE_NAME || "ADMIN";
 const dbPassword = process.env.DATABASE_PASSWORD || "*****";
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server");
 });
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
 
-mongoose.connect(`mongodb+srv://${dbName}:${dbPassword}@cluster0.sazjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(
-    () => {
-        console.log("connected to database");
-    }
-).catch(() => {
+mongoose
+  .connect(
+    `mongodb+srv://${dbName}:${dbPassword}@cluster0.sazjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+  )
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch(() => {
     console.log("connection failed");
-})
+  });
